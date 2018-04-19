c03469d22d22575b4af7c399c0b25c4bb62d9a3b
Ehsan Zargar-Ershadi <ershadi.ehsan@gmail.com>
2018-04-19 11:59:32 +0100

    Show BrowserWindow

---
+++ src/main.js
@@ -0,0 +1,11 @@
+const electron = require('electron');
+
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+app.on('ready',_ =>{
+    new BrowserWindow({
+        height: 400,
+        width: 400
+    });
+});
